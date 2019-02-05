976762520dc9dfb820b40c19f8551cbe2519db54
Francesca Veit <francesca.veit@no-agency.de>
2019-02-05 12:20:46 +0100

    Change some Code

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,14 @@
-alert('Hello world');
+var myName = 'Francesca';
+myName = 'Francesca Veit';
+document.write('Hello my Name is ' + myName + ', nice to meet you!<BR><BR>');
+
+var randomNumber = Math.floor((Math.random() * 6) + 1);
+document.write(randomNumber);
+
+var escapedText = "He said: \"Yes!\", that's for sure.<BR><BR>";
+document.write(escapedText);
+var escapedText2 = 'He said: \\ Yes!, that\'s for sure.<BR><BR>';
+document.write(escapedText2);
+
+
+document.write(typeof randomNumber);
