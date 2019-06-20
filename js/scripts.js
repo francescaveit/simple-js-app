a4a4a3d94e97f0c0a587880bb5e867a451a6518d
Francesca Veit <francesca.veit@no-agency.de>
2019-06-20 15:07:44 +0200

    Change "For" to "forEach" Made some Comments between the Line

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,4 @@
-// var repository = [
-  // [name:'fanyfight', height:7, ['grass', 'poison']],
-  //[name:'jika', height:7, ['grass', 'poison']],
-  //[name:'yave', height:7, ['grass', 'poison']],
-//];
-
+//Pokemon Arr - Name, Size, Type
 var repository = [
   ['Bullbasur',
   0.7,
@@ -16,35 +11,46 @@ var repository = [
   ['Ekans',
   2,
   ['Poison']],
-]
+];
 
+//forEach Function for the output of the value "Type"
+function arrLoopType(currentArr){
+  if (currentArr == 'Grass') {
+    document.write (' <span class="p__small" style="color:rgb(186, 227, 166);">' + currentArr + ',');
+  }
+  else if (currentArr == 'Poison') {
+    document.write (' <span class="p__small" style="color:rgb(106, 42, 106);">' + currentArr + ',');
+  }
+  else if (currentArr == 'Fire') {
+    document.write (' <span class="p__small" style="color:rgb(247, 191, 151);">' + currentArr + ',');
+  }
+  else if (currentArr == 'Flying') {
+    document.write (' <span class="p__small" style="color:rgb(142, 111, 235);">' + currentArr + ',');
+  }
+}
 
-console.log (repository[1][0]);
-
-
-for (var i = 0; i < repository.length; i++) {
-  if (repository[i][1] > 1.9) {
-    document.write ('<p class="top_article">Wow - he is biggest Pokemon</p>');
+//forEach Function for the output of the values Name, Type and Size
+function arrLoop(currentArr){
+  //Ask for the biggest Pokemon
+  if (currentArr[1] > 1.9) {
+    document.write('<p class="top_article">Wow - he is biggest Pokemon</p>');
   }
 
-  document.write ('<H2><a href="">' + repository[i][0] + '</a></H2>');
+  //Name
+  document.write('<H2><a href="">' + currentArr[0] + '</a></H2>');
 
-  document.write ('<p class="p__small">Types: ');
-  for (var j = 0; j < repository[i][2].length; j++) {
-    if (repository[i][2][j] == 'Grass') {
-      document.write (' <span class="p__small" style="color:rgb(186, 227, 166);">' + repository[i][2][j] + ',');
-    }
-    else if (repository[i][2][j] == 'Poison') {
-      document.write (' <span class="p__small" style="color:rgb(106, 42, 106);">' + repository[i][2][j] + ',');
-    }
-    else if (repository[i][2][j] == 'Fire') {
-      document.write (' <span class="p__small" style="color:rgb(247, 191, 151);">' + repository[i][2][j] + ',');
-    }
-    else if (repository[i][2][j] == 'Flying') {
-      document.write (' <span class="p__small" style="color:rgb(142, 111, 235);">' + repository[i][2][j] + ',');
-    }
+  //Size
+  document.write ('<p class="p__small">Size: ' + currentArr[1]);
 
-  }
+  //Type
+  document.write ('<p class="p__small">Type: ');
+  currentArr[2].forEach(arrLoopType);
+
+  //Breaks for the space between the Pokemons
   document.write ('</p>');
   document.write ('<BR><BR>');
+
 }
+
+//Initialization forEach
+repository.forEach(arrLoop);
