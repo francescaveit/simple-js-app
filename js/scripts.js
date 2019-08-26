65520f89fc20c6ac902c0435e1d03bbb38586eac
Francesca Veit <francesca.veit@no-agency.de>
2019-08-26 12:05:41 +0200

    Change the varirbles to a global Variable inside an IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,63 @@
 //Pokemon Arr - Name, Size, Type
-var repository = [
+/*document.write (' <span class="p__small" style="color:rgb(186, 227, 166);">Test</span><BR>');*/
+
+var pokemonRepository = (function (){
+  var repository = [
+    ['Bullbasur',
+    0.7,
+    ['Grass', 'Poison']],
+
+    ['Charizard',
+    1.7,
+    ['Fire', 'Flying']],
+
+    ['Ekans',
+    2,
+    ['Poison']],
+  ];
+
+  function add(pokemon) {
+    repository.push(pokemon);
+    console.log ('Test2: ' + pokemon);
+    };
+
+  function getAll() {
+    return repository;
+    }
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+
+
+var name = 'Pikachu';
+//Initialization forEach
+//repository.forEach(arrLoop);
+
+
+
+//document.write ("1 " + pokemonRepository + '<BR>');
+//document.write ("2 " + pokemonRepository.add() + '<BR>');
+//document.write ("3 " + pokemonRepository.getAll() + '<BR>');
+
+console.log('1: ' + pokemonRepository.getAll()); // []
+//pokemonRepository.add({ name: 'Pikachu' });
+console.log('2: ' + pokemonRepository.getAll()); // [ { name: 'Pikachu' } ]
+
+
+/*pokemonRepository.forEach(function('Bullbasur', 0.7, ['Grass', 'Poison']) {
+    console.log(einArrayElement
+        + ' hat den Index '
+        + derIndex
+        + ' und das Array enthält '
+        + dasArray.length
+        + ' Elemente');
+});*/
+
+
+
+/*var repository = [
   ['Bullbasur',
   0.7,
   ['Grass', 'Poison']],
@@ -11,7 +69,7 @@ var repository = [
   ['Ekans',
   2,
   ['Poison']],
-];
+];*/
 
 //forEach Function for the output of the value "Type"
 function arrLoopType(currentArr){
@@ -52,5 +110,6 @@ function arrLoop(currentArr){
 
 }
 
-//Initialization forEach
-repository.forEach(arrLoop);
+
+//pokemonRepository.forEach(repository);
+pokemonRepository.forEach(repository);
