7740c44425ed574d56fd997d1d4bce32268b7ea4
Francesca Veit <francesca.veit@no-agency.de>
2019-09-11 09:31:51 +0200

    Change forEach to correct version, make some html-output (document.write)

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,4 @@
 //Pokemon Arr - Name, Size, Type
-/*document.write (' <span class="p__small" style="color:rgb(186, 227, 166);">Test</span><BR>');*/
-
 var pokemonRepository = (function (){
   var repository = [
     {
@@ -12,6 +10,11 @@ var pokemonRepository = (function (){
       name: 'Charizard',
       height: 1.7,
       types: ['Fire', 'Flying']
+    },
+    {
+      name: 'Arbok',
+      height: 3.5,
+      types: ['Gift']
     }
 
 
@@ -33,49 +36,26 @@ var pokemonRepository = (function (){
 })();
 
 //Initialization forEach
-//repository.forEach(arrLoop);
-
-
-
-//document.write ("1 " + pokemonRepository + '<BR>');
-//document.write ("2 " + pokemonRepository.add() + '<BR>');
-//document.write ("3 " + pokemonRepository.getAll() + '<BR>');
-//console.log('1: ' + pokemonRepository.getAll()); // []
-//pokemonRepository.add({ name: 'Pikachu' });
-//console.log('2: ' + pokemonRepository.getAll()); // [ { name: 'Pikachu' } ]
-
 pokemonRepository.getAll().forEach(element => {
-  console.log ('Show me the Element' + element);
-  console.log (element);
-  document.write(element);
-  //document.write (element);
+  if (element.height > 1.9) {
+    document.write('<p class="top_article">Wow - he is biggest Pokemon</p>');
+  }
+
+  //Name
+  document.write('<H2><a href="">' + element.name + '</a></H2>');
+
+  //Size
+  document.write ('<p class="p__small">Size: ' + element.height);
+
+  //Type
+  document.write ('<p class="p__small">Type: ');
+  element.types.forEach(arrLoopType);
+
+  //Breaks for the space between the Pokemons
+  document.write ('</p>');
+  document.write ('<BR><BR>');
   }
 );
-
-/*pokemonRepository.forEach(function('Bullbasur', 0.7, ['Grass', 'Poison']) {
-    console.log(einArrayElement
-        + ' hat den Index '
-        + derIndex
-        + ' und das Array enthält '
-        + dasArray.length
-        + ' Elemente');
-});*/
-
-
-
-/*var repository = [
-  ['Bullbasur',
-  0.7,
-  ['Grass', 'Poison']],
-
-  ['Charizard',
-  1.7,
-  ['Fire', 'Flying']],
-
-  ['Ekans',
-  2,
-  ['Poison']],
-];*/
 
 //forEach Function for the output of the value "Type"
 function arrLoopType(currentArr){
@@ -91,31 +71,7 @@ function arrLoopType(currentArr){
   else if (currentArr == 'Flying') {
     document.write (' <span class="p__small" style="color:rgb(142, 111, 235);">' + currentArr + ',');
   }
-}
-
-//forEach Function for the output of the values Name, Type and Size
-function arrLoop(currentArr){
-  //Ask for the biggest Pokemon
-  if (currentArr[1] > 1.9) {
-    document.write('<p class="top_article">Wow - he is biggest Pokemon</p>');
+  else if (currentArr == 'Gift') {
+    document.write (' <span class="p__small" style="color:rgb(255, 0, 0);">' + currentArr + ',');
   }
-
-  //Name
-  document.write('<H2><a href="">' + currentArr[0] + '</a></H2>');
-
-  //Size
-  document.write ('<p class="p__small">Size: ' + currentArr[1]);
-
-  //Type
-  document.write ('<p class="p__small">Type: ');
-  currentArr[2].forEach(arrLoopType);
-
-  //Breaks for the space between the Pokemons
-  document.write ('</p>');
-  document.write ('<BR><BR>');
-
 }
-
-
-//pokemonRepository.forEach(repository);
-//pokemonRepository.forEach(repository);
